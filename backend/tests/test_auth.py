a6b7309710f201bd8677d71846f886240f77a374
Kevin Liu <kevinsliu0@gmail.com>
2022-11-19 16:07:40 -0800

    Wrap everything in docker compose

--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -1,6 +1,9 @@
 from requests import get, post
 
 
-r = post("http://localhost:5000/api/auth/login", data={"username":"Jane", "password":"Doe", "userType":"Student"})
+r = post(
+    "http://localhost:5000/api/auth/login",
+    data={"username": "Jane", "password": "Doe", "userType": "Student"},
+)
 
 print(r.text)
--- backend/tests/test_mongo.py
+++ backend/tests/test_mongo.py
@@ -1,16 +1,16 @@
 from pymongo import MongoClient
 
-client = MongoClient('localhost', 27017, username="root", password="rootpassword")
+client = MongoClient("localhost", 27017, username="root", password="rootpassword")
 
 db = client.db
 users = db.users
 
 # users.insert_one({'username': "Jane", 'password': "Doe", 'type': 'Student'})
 
-print(users.find_one({"username":"Jane"}))
+print(users.find_one({"username": "Jane"}))
 
 for k in users.find({"username": "Jane"}):
     print(k)
 
-print(users.count_documents({"username":"Jane"}))
+print(users.count_documents({"username": "Jane"}))
 client.close()
